eb3f5510f2ddbe66a5d27c9bb0fac7ad95110ab2
Khalid Fahmy <53705950+khalidfahmy98@users.noreply.github.com>
2020-09-09 11:00:50 +0200

    updated loadbalancer functionality to be dynamic

--- loadbalancer.js
+++ loadbalancer.js
@@ -1,13 +1,39 @@
 $(document).ready(function () {
-    // focusing script to toggle the wrapper 
     let startId = 1, endId = 500;
-    let container = $(".balancer-data-wrapper");
     let t = 2, newEnd = 0;
+    //changeable variable values 
+    let container = $(".balancer-data-wrapper");
     let searchContainer = $(".balancer-results-wrapper");
 
-    $(".balancer-main-input").focus(function () {
-        $(this).siblings(".balancer-inner-wrapper").slideDown();
+    //dynamic scripts 
+    //open dropdownlist content container 
+    $(".balancer-main-input").click(function () {
+        let iconToggler = $(this).siblings(".balancer-icon");
+        if (iconToggler.hasClass("fa-caret-down")) {
+            $(this).siblings(".balancer-inner-wrapper").slideDown();
+            $(this).siblings(".balancer-inner-wrapper").children("header").children(".balancer-searcher").focus();
+            iconToggler.removeClass("fa-caret-down").addClass("fa-caret-up");
+        } else {
+            $(this).siblings(".balancer-inner-wrapper").slideUp();
+            iconToggler.addClass("fa-caret-down").removeClass("fa-caret-up");
+        }
     });
+    //  activate choosed data from balancer requested data 
+    $(".data-floater").on("click", ".balancer-item", function () {
+        $(this).addClass("active-item").siblings(".balancer-item").removeClass("active-item");
+        $(this).parent("div").parent(".balancer-inner-wrapper").siblings(".balancer-main-input").html($(this).data("requestedname") + " - " + $(this).data("barcode"));
+        $(this).parent("div").parent(".balancer-inner-wrapper").siblings(".balancer-main-input").data("itemid", $(this).data("requestedid"));
+        $(this).parent("div").parent(".balancer-inner-wrapper").slideUp();
+        $(this).parent("div").parent(".balancer-inner-wrapper").siblings(".balancer-icon").removeClass("fa-caret-up").addClass("fa-caret-down");
+    });
+    $(".balancer-inner-wrapper").on("click", ".closeSearch", function () {
+        $(this).parent("header").siblings(".balancer-data-wrapper").show();
+        $(this).parent("header").siblings(".balancer-results-wrapper").hide();
+        $(this).hide();
+    });
+
+
+    //static calls scripts  change only the ajax calls URL APIS
     $.ajax({
         url: "/Item/APIItem?StartId=" + startId + "&EndId=" + endId,
         type: 'Get',
@@ -42,21 +68,13 @@ $(document).ready(function () {
             });
         }
     });
-    //  activate choosed data from balancer requested data 
-
-    $(".data-floater").on("click", ".balancer-item", function () {
-        $(this).addClass("active-item").siblings(".balancer-item").removeClass("active-item");
-        $(".balancer-main-input").val($(this).data("requestedname") + " - " + $(this).data("barcode"));
-        $(".balancer-main-input").data("itemid", $(this).data("requestedid"));
-        $(".balancer-inner-wrapper").slideUp();
-
-    });
     // search script 
     let focusTrigger = 0; 
     $(".balancer-searcher").focus(function () {
         focusTrigger++;
-        $(".balancer-data-wrapper").hide();
-        $(".balancer-results-wrapper").show();
+        $(this).parent("header").siblings(".balancer-data-wrapper").hide();
+        $(this).parent("header").siblings(".balancer-results-wrapper").show();
+        $(this).siblings(".closeSearch").show();
         if (focusTrigger <= 1   ) {
             var values = [],
                 keys = Object.keys(localStorage),
@@ -73,8 +91,9 @@ $(document).ready(function () {
     $(".balancer-searcher").keypress(function (e) {
         if (e.which == 13) {
             if ($(this).val() == "") {
-                $(".balancer-data-wrapper").show();
-                $(".balancer-results-wrapper").hide();
+                $(this).parent("header").siblings(".balancer-data-wrapper").show();
+                $(this).parent("header").siblings(".balancer-results-wrapper").hide();
+                $(this).siblings(".closeSearch").show();
             } else {
                 $.ajax({
                     url: "/Item/APIItemSearch?text=" + $(this).val(),
@@ -97,7 +116,7 @@ $(document).ready(function () {
         var itemObject = { 'id': id, 'barcode': barcode, 'name': name };
         localStorage.setItem('item-' + id, JSON.stringify(itemObject));
         searchContainer.append('<span class="balancer-item " data-barcode="' + barcode + '" data-requestedid="' + id + '" data-requestedname="' + name + '">' + name + '-' + barcode + '</span>');
-       $(".balancer-inner-wrapper").slideUp();
+        $(this).parent(".balancer-results-wrapper").parent(".balancer-inner-wrapper").slideUp();
 
     });
 
